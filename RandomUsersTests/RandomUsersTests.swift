d2a379b78c0b01fa95e30ca567950c16c7fb10f6
Ben Stone <benjamin.h.stone@gmail.com>
2021-09-09 17:42:55 -0400

    add test

--- RandomUsersTests/RandomUsersTests.swift
+++ RandomUsersTests/RandomUsersTests.swift
@@ -9,8 +9,22 @@ import XCTest
 @testable import RandomUsers
 
 class RandomUsersTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testUserModel() throws {
+        let jsonData = getTestJSONData()
+        
+        do {
+            _ = try JSONDecoder().decode(Response.self, from: jsonData)
+        }
+        catch {
+            XCTFail("Failed to decode JSON into the model: \(error)")
+        }
+    }
+    
+    private func getTestJSONData() -> Data {
+        guard let path = Bundle.main.path(forResource: "randomUsers", ofType: "json") else {
+            fatalError("randomUsers.json file not found")
+        }
+        let internalURL = URL(fileURLWithPath: path)
+        return try! Data(contentsOf: internalURL)
     }
 }
