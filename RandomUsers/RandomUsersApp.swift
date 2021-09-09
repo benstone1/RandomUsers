c155f308411c310802655e3f091fbb7b7bbe77a3
Ben Stone <benjamin.h.stone@gmail.com>
2021-09-09 15:52:32 -0400

    initial commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Ben Stone on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
